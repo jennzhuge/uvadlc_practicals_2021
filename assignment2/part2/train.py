b8b670cc0c2e38c50f78b8573a9ed100255d89d6
jennzhuge <90708293+jennzhuge@users.noreply.github.com>
2021-12-02 22:58:34 +0100

    hi
    hi

--- assignment2/part2/train.py
+++ assignment2/part2/train.py
@@ -125,13 +125,13 @@ def train(args):
         print(epoch)
     
     # save model
-    filename = 'lstm_model.sav'
+    filename = 'lstm_model_sample.sav'
     pickle.dump(model, open(filename, 'wb'))
     
     # save loss and accuracy
     results = {'loss': train_loss, 'accs': train_accuracies}
     print(results)
-    torch.save(results, 'lossAcc.txt')
+    torch.save(results, 'lossAcc_sample.txt')
         
     #######################
     # END OF YOUR CODE    #
