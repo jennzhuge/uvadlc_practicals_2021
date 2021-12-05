b195305b7e9e014aa739a1f1834be3ac413e930b
jennzhuge <90708293+jennzhuge@users.noreply.github.com>
2021-12-05 11:28:51 +0100

    hi
    hi

--- assignment2/part2/train.py
+++ assignment2/part2/train.py
@@ -162,6 +162,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     #args.device = "cpu"
     args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Use GPU if available, else use CPU
-    args.sampling = True
+    #args.sampling = True
     train(args)
 
